60321a6947e74c80f5316a9f11d3b402c8f78edf
hannah <hannah13jack@gmail.com>
2025-12-27 20:52:32 +0000

    Calculator app

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.Diagnostics.CodeAnalysis;
+
+Console.WriteLine("Enter a number!");
+
+double num1 = 0;
+
+string userInput  = Console.ReadLine();
+
+num1 = double.Parse(userInput);
+
+Console.WriteLine("Enter another number!");
+
+double num2 = 0;
+
+string userInput2 = Console.ReadLine();
+
+num2 = double.Parse(userInput2);
+
+double sum = num1 + num2;
+sum = Math.Round(sum, 2);
+
+Console.WriteLine($"{num1} + {num2} = {sum}");
+Console.ReadKey();
