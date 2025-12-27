8999ed7bd9aa8516b5f290083faa2132be6580e9
hannah <hannah13jack@gmail.com>
2025-12-27 23:07:57 +0000

    Update LogicApp

--- LogicApp/LogicApp/Program.cs
+++ LogicApp/LogicApp/Program.cs
@@ -1,55 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-int num1 = 0;
-int num2 = 0;
+Console.WriteLine("How old are you?");
+int age = int.Parse(Console.ReadLine());
+bool isWithParents = false;
 
-bool isEqual = num1 == num2;
-
-bool isNotEqual = num1 != num2;
-
-Console.WriteLine("Please enter a whole number");
-
-if (num1 == int.Parse(Console.ReadLine()))
+if (age >= 18)
 {
-    Console.WriteLine("Numbers are equal!");
-
-    Console.WriteLine("Please enter your age");
-    int age = int.Parse(Console.ReadLine());
-    if (age >= 18)
+    Console.WriteLine("Go party in the club!");
+}
+else if (age >= 13)
+{
+    Console.WriteLine("Are you with parents? Answer with y or n");
+    string isWithParentsString = Console.ReadLine();
+    if (isWithParentsString == "y")
     {
-        Console.WriteLine("Please enter your address, " +
-            "so that we can send you the price!");
-        string address = Console.ReadLine();
+        Console.WriteLine("Go party in the club with your parents!");
     }
     else
     {
-        Console.WriteLine("Sorry, you can't get your price due to your age!");
+        Console.WriteLine("No party for you");
     }
-
-}
-else
-{
-    Console.WriteLine("Numbers aren't equal");
-}
-
-/*
-
-int age = 16;
-bool isWithParents = true;
-
-if(age >= 13 && isWithParents)
-{
-    Console.WriteLine("Go party in the club with your parents!");
-}else if (age > 18)
-{
-    Console.WriteLine("Go party in the club!");
 }
 else
 {
     Console.WriteLine("Go party in Kindergarten!");
 }
 
-*/
 
 Console.ReadKey();
 
