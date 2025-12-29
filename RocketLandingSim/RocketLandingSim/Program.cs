6cc90ef9ed4fb63f1a4df0731ff9d2e3ccc26320
hannah <hannah13jack@gmail.com>
2025-12-29 21:01:01 +0000

    RocketLandingSim

---
+++ RocketLandingSim/RocketLandingSim/Program.cs
@@ -0,0 +1,14 @@
+﻿string rocket = "  |\r\n / \\\r\n*---*\r\n| o |\r\n| o |\r\n*---*\r\n /_\\";
+
+for (int i = 10; i >= 0; i--)
+{
+    Console.WriteLine( rocket );
+    rocket = "\n" + rocket;
+    Thread.Sleep( 500 );
+    Console.Clear();
+}
+
+Console.WriteLine( rocket );
+Console.WriteLine( "Successful landing !" );
+
+Console.ReadKey();
