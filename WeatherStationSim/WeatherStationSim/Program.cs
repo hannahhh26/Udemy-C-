7d469163ccb446b3a6f5e89174d2e06a8d75c3d2
hannah <hannah13jack@gmail.com>
2025-12-30 19:22:05 +0000

    WeatherStationSim update

--- WeatherStationSim/WeatherStationSim/Program.cs
+++ WeatherStationSim/WeatherStationSim/Program.cs
@@ -12,7 +12,7 @@ namespace WeatherStationSim
             int days = int.Parse(Console.ReadLine());
 
             int[] temperature = new int[days];
-            string[] conditions = {"Sunny", "Rainy", "Cloudy", "Snowy"};
+            string[] conditions = { "Sunny", "Rainy", "Cloudy", "Snowy" };
             string[] weatherConditions = new string[days];
 
             Random random = new Random();
@@ -22,20 +22,51 @@ namespace WeatherStationSim
                 temperature[i] = random.Next(-10, 40);
                 weatherConditions[i] = conditions[random.Next(conditions.Length)];
             }
-            int averageTemp = AverageTemp(temperature);
-            Console.WriteLine(averageTemp);
+
+            Console.WriteLine($"Max temp: {temperature.Max()}");
+            Console.WriteLine($"Min temp: {temperature.Min()}");
+
+            double averageTemp = AverageTemp(temperature);
+            Console.WriteLine($"Average temp: {averageTemp}");
             Console.ReadKey();
 
-            static int AverageTemp(int[] temperature)
-            {
-                int sum = 0;
-                foreach (int temp in temperature)
-                {
-                    sum += temp;
-                }
-                return sum;
-            }
+        }
 
+        static string MostCommonCondition(string[] conditions)
+        {
+            int count = 0;
+            string mostCommon = conditions[0];
+
+            for (int i = 0; i < conditions.Length; i++)
+            {
+                int tempCount = 0;
+                for (int j = 0; j < conditions.Length; j++)
+                {
+                    if (conditions[j] == conditions[i])
+                    {
+                        tempCount++;
+
+                    }
+                }
+                if (tempCount > count)
+                {
+                    mostCommon = conditions[i];
+                }
+            }
+            return mostCommon;
+        }
+
+        static double AverageTemp(int[] temperature)
+        {
+            double sum = 0;
+            foreach (int temp in temperature)
+            {
+                sum += temp;
+            }
+            double average = sum / temperature.Length;
+            return average;
         }
     }
+
+
 }
