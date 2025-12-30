60eaf74734813830388a20912d4666e3d8c37dd1
hannah <hannah13jack@gmail.com>
2025-12-30 19:17:15 +0000

    WeatherStationSim

---
+++ WeatherStationSim/WeatherStationSim/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Runtime.ExceptionServices;
+
+namespace WeatherStationSim
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of days to simulate:");
+            int days = int.Parse(Console.ReadLine());
+
+            int[] temperature = new int[days];
+            string[] conditions = {"Sunny", "Rainy", "Cloudy", "Snowy"};
+            string[] weatherConditions = new string[days];
+
+            Random random = new Random();
+
+            for (int i = 0; i < days; i++)
+            {
+                temperature[i] = random.Next(-10, 40);
+                weatherConditions[i] = conditions[random.Next(conditions.Length)];
+            }
+            int averageTemp = AverageTemp(temperature);
+            Console.WriteLine(averageTemp);
+            Console.ReadKey();
+
+            static int AverageTemp(int[] temperature)
+            {
+                int sum = 0;
+                foreach (int temp in temperature)
+                {
+                    sum += temp;
+                }
+                return sum;
+            }
+
+        }
+    }
+}
