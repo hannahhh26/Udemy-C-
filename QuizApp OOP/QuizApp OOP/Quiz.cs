41d2e2e99fcf4eaca2dd2bbaca15b38fc7748453
hannah <hannah13jack@gmail.com>
2026-01-09 13:36:26 +0000

    OOP Quiz app

---
+++ QuizApp OOP/QuizApp OOP/Quiz.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizApp_OOP
+{
+    internal class Quiz
+    {
+        private Question[] _questions;
+        private int _score;
+
+
+        public Quiz(Question[] questions)
+        {
+            this._questions = questions;
+            _score = 0;
+        }
+
+        public void StartQuiz()
+        {
+            Console.WriteLine("Welcome to the Quiz!");
+            int questionNumber = 1;
+
+            foreach (Question question in _questions)
+            {
+                Console.WriteLine($"Question {questionNumber++}");
+                DisplayQuestion(question);
+                int userChoice = GetUserChoice();
+                if (question.IsCorrectAnswer(userChoice))
+                {
+                    Console.WriteLine("Correct");
+                    _score++;
+                }
+                else
+                {
+                    Console.WriteLine($"Wrong! The correct answer was: {question.Answers[question.CorrectAnswerIndex]}");
+
+                    double percentage = (double)_score / _questions.Length;
+                    if (percentage >= 0.8)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Excellent Work");
+                    }
+                    else if (percentage >= 0.5)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("Good effort!");
+                    }
+                    else
+                    {
+                        Console.ForegroundColor= ConsoleColor.Red;
+                        Console.WriteLine("Keep practicing");
+                    }
+                    Console.ResetColor();
+
+                }
+                DisplayResults();
+
+            }
+        }
+
+        private void DisplayResults()
+        {
+            Console.WriteLine($"Quiz finished. Your score is {_score} out of {_questions.Length}");
+        }
+
+        private void DisplayQuestion(Question question)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("╔═════════════════════════════════════════════════════════════════════════╗");
+            Console.WriteLine("║                                 Question                                ║");
+            Console.WriteLine("╚═════════════════════════════════════════════════════════════════════════╝");
+            Console.ResetColor();
+            Console.WriteLine(question.QuestionText);
+
+            for (int i = 0; i < question.Answers.Length; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                Console.Write("   ");
+                Console.Write(i + 1);
+                Console.ResetColor();
+                Console.WriteLine($". {question.Answers[i]}");
+            }
+
+        }
+
+        private int GetUserChoice()
+        {
+            Console.Write("Your answer (number): ");
+            string input = Console.ReadLine();
+            int choice = 0;
+            while (!int.TryParse(input, out choice) || choice < 1 || choice > 4)
+            {
+                Console.WriteLine("Invalid choice. Please enter a number between 1 and 4: ");
+                input = Console.ReadLine();
+            }
+
+            return choice - 1; // adjust to 0-indexed array
+
+        }
+    }
+}
