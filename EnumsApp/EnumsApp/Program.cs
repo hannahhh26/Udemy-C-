41bba39fb34f055c177fecef42e43069e197a23d
hannah <hannah13jack@gmail.com>
2026-01-27 23:22:25 +0000

    EnumsApp

---
+++ EnumsApp/EnumsApp/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace EnumsApp
+{
+    internal class Program
+    {
+        enum Day { Mo, Tu, We, Th, Fr, Sa, Su};
+
+        static void Main(string[] args)
+        {
+            Day fr = Day.Fr;
+            Day su = Day.Su;
+
+            Day a = Day.Fr;
+
+            Console.WriteLine(fr==a);
+            Console.WriteLine((int)Day.Mo);
+            Console.ReadKey();
+        }
+
+        
+    }
+}
