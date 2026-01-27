8b1e413850316ceed3c11935e2e4459d69d0218c
hannah <hannah13jack@gmail.com>
2026-01-27 15:17:10 +0000

    InterfacesApp

---
+++ InterfacesApp/InterfacesApp/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace InterfacesApp
+{
+    public class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Generic sound");
+        }
+    }
+
+    class Dog() : Animal
+    {
+        public void Eat(string food)
+        {
+            Console.WriteLine($"Dog ate {food}");
+        }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine("bark");
+        }
+    }
+
+    class Cat() : Animal
+    {
+        public void Eat(string food)
+        {
+            Console.WriteLine($"Cat ate {food}");
+        }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine("meow");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal myDog = new Dog();
+        }
+    }
+}
