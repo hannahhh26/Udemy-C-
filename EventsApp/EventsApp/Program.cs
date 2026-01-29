7d00c94f6d5198c247de3331354fefd560d38459
hannah <hannah13jack@gmail.com>
2026-01-29 14:37:33 +0000

    EventsApp

---
+++ EventsApp/EventsApp/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace EventsApp
+{
+    public delegate void Notify(string message);
+
+    public class EventPublisher
+    {
+        // Use "On" for events
+        public event Notify OnNotify;
+
+        public void RaiseEvent(string message)
+        {
+            OnNotify?.Invoke(message);
+        }
+    }
+
+    public class EventSubscriber
+    {
+        public void OnEventRaised(string message)
+        {
+            Console.WriteLine($"Event recieved: {message}");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EventPublisher publisher = new EventPublisher();
+            EventSubscriber subsriber = new EventSubscriber();
+            publisher.OnNotify += subsriber.OnEventRaised;
+
+            publisher.RaiseEvent("test"); 
+
+            Console.ReadKey();
+        }
+    }
+}
