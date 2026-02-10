9eb9d62656723e4c02316c42c6f513f9ec293544
hannah <hannah13jack@gmail.com>
2026-02-10 17:47:08 +0000

    Add RegexApp

---
+++ RegexApp/RegexApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegexApp
+{
+    internal class Program
+    {
+        public static void ExtractPattners(string input)
+        {
+            Regex regex = new Regex(@"[a-zA-Z0-9]+?@\w+?\.\w+");
+            MatchCollection matches = regex.Matches(input);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match);
+            }
+        }
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            //string pattern = @"\d";
+            //Regex regex = new Regex(pattern);
+
+            //string text = "Hi there, my number is 1234";
+
+            //MatchCollection matches = regex.Matches(text);
+
+            //Console.WriteLine($"{matches.Count} hits found: \n {text}");
+
+            //foreach (Match hit in matches)
+            //{
+            //    GroupCollection group = hit.Groups;
+            //    Console.WriteLine($"{group[0].Value} found at {group[0].Index}");
+            //}
+            ExtractPattners("Contact us at support@example.com");
+
+            
+
+            Console.ReadKey();
+
+        }
+    }
+}
